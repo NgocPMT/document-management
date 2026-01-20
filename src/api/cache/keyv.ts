b2ea6b722d502ffb21feb46e68e70c4469346de9
Nathan Pham <np1552005@gmail.com>
2026-01-20 08:45:16 +0000

    chore(cache): add cloud redis url

--- src/api/cache/keyv.ts
+++ src/api/cache/keyv.ts
@@ -1,13 +1,17 @@
 import Keyv from "keyv";
 import KeyvRedis from "@keyv/redis";
+import { secret } from "encore.dev/config";
+
+const redisURL = secret("REDIS_URL");
 
 export const cache = new Keyv({
   store: new KeyvRedis({
-    url: "redis://localhost:6379",
+    url: redisURL(),
   }),
   ttl: 60_000, // 1 Minutes
 });
 
 cache.on("error", (err) => {
+  console.error(err);
   throw new Error("Keyv Connection Error", err);
 });
