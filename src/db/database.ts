d23f48c4e16e82ebf9896b4f55580a7ba5a3a1bc
Nathan Pham <np1552005@gmail.com>
2026-01-22 01:26:52 +0000

    perf(db): enable connection pooling

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,6 +1,7 @@
 import { SQLDatabase } from "encore.dev/storage/sqldb";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
+import { Pool } from "pg";
 
 const DB = new SQLDatabase("document-management", {
   migrations: {
@@ -9,4 +10,13 @@ const DB = new SQLDatabase("document-management", {
   },
 });
 
-export const db = drizzle(DB.connectionString, { schema });
+const pool = new Pool({
+  connectionString: DB.connectionString,
+  max: 15,
+  min: 2,
+  idleTimeoutMillis: 30_000, // 30 Seconds,
+  connectionTimeoutMillis: 2_000, // 2 Seconds
+  keepAlive: true,
+});
+
+export const db = drizzle(pool, { schema });
